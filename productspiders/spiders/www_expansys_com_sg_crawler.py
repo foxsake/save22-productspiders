3182952a86f2c53dbbf409e6db910aef733e392c
Paul Austian Alday <paul.austian.alday@gmail.com>
2016-03-03 08:47:37 +0800

    make spider crawl main page

--- productspiders/spiders/www_expansys_com_sg_crawler.py
+++ productspiders/spiders/www_expansys_com_sg_crawler.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from productspiders.items import ProductspidersItem
 
 class WwwExpansysComSgCrawler(scrapy.Spider):
     name = "www_expansys_com_sg_crawler"
     allowed_domains = ["www.expansys.com.sg"]
     start_urls = (
-        'http://www.www.expansys.com.sg/',
+        'http://www.expansys.com.sg/',
     )
 
     def parse(self, response):
+        for ln in response.xpath('//*[@class="nitem"]/@href'):
+            print ln
+        # for pr in response.xpath('//*[@id="product"]'):
+        #     item = ProductspidersItem()
+        #     item["title"] = pr.xpath('//*[@id="title"]/h1/text()').extract()
+        #     item["url"] = pr.xpath("a/@href").extract()
+        #     yield item
         pass
