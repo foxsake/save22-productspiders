67941caa88387285a9b2986f58e6cfa0cede8cc2
Paul Austian Alday <paul.austian.alday@gmail.com>
2016-03-03 10:24:01 +0800

    make spider read product titles

--- productspiders/spiders/www_expansys_com_sg_crawler.py
+++ productspiders/spiders/www_expansys_com_sg_crawler.py
@@ -12,10 +12,18 @@ class WwwExpansysComSgCrawler(scrapy.Spider):
 
     def parse(self, response):
         for ln in response.xpath('//*[@class="nitem"]/@href'):
-            print ln
+            # print ln.extract()
+            yield scrapy.Request(ln.extract(), callback=self.parse_nitem)
         # for pr in response.xpath('//*[@id="product"]'):
         #     item = ProductspidersItem()
         #     item["title"] = pr.xpath('//*[@id="title"]/h1/text()').extract()
         #     item["url"] = pr.xpath("a/@href").extract()
         #     yield item
-        pass
+    
+    def parse_nitem(self,response):
+        print 'looking in..'
+        # for ln in response.xpath('//*[@id="product_listing"]/div/ul'):
+        for ln in response.xpath('//*[@id="product_listing"]/div/ul/li[@class="title"]/h3/a/text()'):
+            print ln.extract()
+            # print ln.xpath('//*[@id="product_listing"]/div/ul/li[@class="title"]/h3/a/text()').extract()
+            # print ln.xpath('//*[@id="product_listing"]/div/ul/li[@class="title"]/h3/a/@href').extract()
